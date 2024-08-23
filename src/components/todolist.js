eabdf110f626b7559bfa7245972f0427774908b1
TshepoMadira <124384592+TshepoMadira@users.noreply.github.com>
2024-08-23 11:12:10 +0200

    Add files via upload
    first commit

--- src/components/todolist.js
+++ src/components/todolist.js
@@ -1,19 +1,52 @@
 import { useSelector, useDispatch } from "react-redux";
-import {deleteTodoItem } from  '../redux/todoListReducer'
+import { deleteTodoItem, editTodoItem } from '../redux/todoListReducer'
+import { useState } from 'react';
 
 function TodoList() {
     const todoList = useSelector(state => state.todoList);
 
     const dispatch = useDispatch() ;
 
+    const [newTodoItem, setNewTodoItem] = useState('');
+    const [editingItemId, setEditingItemId] = useState(null);
+
+    const handleEdit = (id, updates) => {
+        dispatch(editTodoItem({ id, updates }));
+    }
+
+    const handleInputChange = (id, e) => {
+        if (id === editingItemId) {
+            setNewTodoItem(e.target.value);
+        }
+    }
+
+    const handleEditClick = (id) => {
+        setEditingItemId(id);
+        setNewTodoItem(todoList.find(item => item.id === id).todoItem);
+    }
+
+    const handleSaveClick = (id) => {
+        handleEdit(id, { todoItem: newTodoItem });
+        setNewTodoItem('');
+        setEditingItemId(null);
+    }
 
     return (
         <ul>
             {todoList.map((item) => (
                 <div>
-            
-                <li key={item.id}>{item.todoItem}</li>
-                <button onClick={()=> dispatch(deleteTodoItem(item.id))}>delete</button>
+                    <li key={item.id}>{item.todoItem}</li>
+                    <button onClick={()=> dispatch(deleteTodoItem(item.id))}>delete</button>
+                    {editingItemId === item.id ? (
+                        <input type="text" value={newTodoItem} onChange={(e) => handleInputChange(item.id, e)} />
+                    ) : (
+                        <span>{item.todoItem}</span>
+                    )}
+                    {editingItemId === item.id ? (
+                        <button onClick={() => handleSaveClick(item.id)}>save</button>
+                    ) : (
+                        <button onClick={() => handleEditClick(item.id)}>edit</button>
+                    )}
                 </div>
             ))}
         </ul>
--- src/redux/todoListReducer.js
+++ src/redux/todoListReducer.js
@@ -10,13 +10,26 @@ const todoSlice = createSlice({
         } ,
         deleteTodoItem : (state, action) =>{
        return state.filter(list=> list.id !==action.payload)
-
         },
+        editTodoItem : (state, action) =>{
+            const {id, updates} = action.payload;
+            return state.reduce((acc, item)=>{
+                if (item.id==id){
+                    acc.push({...item,...updates});
+
+
+                }else{
+                    acc.push(item);
+                }
+                return acc;
+            }, []);
+            
+        }
 
 
     }
 });
 
 
-export const { addTodoItem , deleteTodoItem} = todoSlice.actions;
+export const { addTodoItem , deleteTodoItem, editTodoItem} = todoSlice.actions;
 export default todoSlice.reducer;
